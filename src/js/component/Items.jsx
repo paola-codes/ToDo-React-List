fadd14d1f1605618335440f1dea8e4c6fb4962dd
paola9896 <paolasc2652@gmail.com>
2021-09-28 11:29:51 +0000

    changes

---
+++ src/js/component/Items.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const Items = () => {
+    const [item, setitem] = React.useState('green');
+    return (
+        
+    )
+}
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,13 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { Items } from "./Items";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
+export const Home = () => {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="style1">
+			<Items />
 		</div>
 	);
 };
 
-export default Home;
+
